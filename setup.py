031bd5b94e66c1dc5b6a05f59f8e37c217405a24
Leonardo Lazzaro <llazzaro@dc.uba.ar>
2016-01-22 00:35:54 -0300

    pep8 fixes and compatibility with latest nltk

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 from setuptools import setup
-import os
 
 try:
-   import pypandoc
-   long_description = pypandoc.convert('README.md', 'rst')
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
 except (IOError, ImportError):
-   long_description = open('README.md').read()
+    long_description = open('README.md').read()
 
 setup(name='geograpy',
       version='0.3.7',
       description='Extract countries, regions and cities from a URL or text',
       long_description=long_description,
       url='https://github.com/ushahidi/geograpy',
-      download_url ='https://github.com/ushahidi/geograpy/tarball/0.3.4',
+      download_url='https://github.com/ushahidi/geograpy/tarball/0.3.4',
       author='Jonathon Morgan',
       author_email='jonathon@ushahidi.com',
       license='MIT',
@@ -25,7 +24,7 @@ setup(name='geograpy',
             'pycountry'
       ],
       scripts=['geograpy/bin/geograpy-nltk'],
-      package_data = {
+      package_data={
             'geograpy': ['data/*.csv'],
       },
       zip_safe=False)
